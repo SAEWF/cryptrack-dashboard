f35243501df5ab464d5f13d9c08df82208831c49
RAGHAV MUKATI <mukati_951955@student.nitw.ac.in>
2022-08-26 09:34:59 +0530

    test

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,6 +9,7 @@ import Header from './Header';
 import './Home.scss';
 import { baseUrl } from '../shared/baseUrl';
 const ariaLabel = { 'aria-label': 'description' };
+const metadataKeys = { fileUrl: 'fileUrl', file_id: 'fileId', file_name: 'fileName', latitude: 'latitude', longitude: 'longitude', msg: 'Enter Message', receiver: "Enter Receiver Email", sender: "Enter Sender Email", time: "Enter time in milliseconds" };
 
 const Home = (props) => {
     const navigate = useNavigate();
@@ -22,6 +23,7 @@ const Home = (props) => {
     // once integrate with server, remove this hardcoded data to empty array
     const [trackedData, setTrackedData] = useState([]);
     const [expanded, setExpanded] = useState(-1)
+    const [formData, setFormData] = useState({ fileUrl: '', file_id: '', file_name: '', latitude: '', longitude: '', msg: '', receiver: "", sender: "", time: "" });
 
     useEffect(() => {
         if (props.loggedIn === false || props.user === null) {
@@ -33,28 +35,34 @@ const Home = (props) => {
         try {
             e.preventDefault();
             setLoading(true);
+            let jsonBody = formData;
+            jsonBody = { ...jsonBody, attachmentData: { fileUrl: formData.fileUrl, file_id: formData.file_id, file_name: formData.file_name } };
+            delete jsonBody.fileUrl;
+            delete jsonBody.file_id;
+            delete jsonBody.file_name;
             // @shubhank fetch tracking data using user, msg and time payload and
-            const response = await fetch(baseUrl+'users/generateAPIKey', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': 'Bearer ' + props.token
-                },
-                body: JSON.stringify({username: props.user['username']})
-            });
-            const data1 = await response.json();
-            console.log(data1);
-            const key = data1.apiKey;
-            const secret = data1.apiSecret;
+            // const response = await fetch(baseUrl+'users/generateAPIKey', {
+            //     method: 'POST',
+            //     headers: {
+            //         'Content-Type': 'application/json',
+            //         'Authorization': 'Bearer ' + props.token
+            //     },
+            //     body: JSON.stringify({username: props.user['username']})
+            // });
+            // const data1 = await response.json();
+            // console.log(data1);
+            // const key = data1.apiKey;
+            // const secret = data1.apiSecret;
+            console.log(jsonBody);
             const resp = await fetch(baseUrl+'track',{
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
-                    'apikey': key, 
-                    'apisecret': secret, 
-                    'username': props.user.username
+                    'apikey': 'L.W6w9.XpGOYb1iKGfhmW3q0', 
+                    'apisecret': 'QrwygxP8_32GTSjuXayl_TrAJOuYiGvBYLzb', 
+                    'username': 'saewf@nitw.com'
                 },
-                body: JSON.stringify({msg,sender,receiver,time})
+                body: JSON.stringify(jsonBody),
             });
             const data = await resp.json();
             console.log(data);
@@ -67,13 +75,27 @@ const Home = (props) => {
         }
     }
     
+    const handleChange = (key, value) => {
+        setFormData(prev => { return { ...prev, [key]: value } })
+    };
+
+    const getInputs = () => {
+        let inputs = [];
+        for (let k in metadataKeys) {
+            inputs.push(<li>{k}: <input value={formData[k]} key={k} onChange={(e) => handleChange(k, e.target.value)} placeholder={metadataKeys[k]} /></li>)
+        }
+        return <ul>
+            {inputs.map(i => i)}
+        </ul>;
+    }
+
     return (
         <>
             <Header loggedIn={props.loggedIn} user={props.user} setUser={props.setUser} setLoggedIn={props.setLoggedIn}/>
             <div className="home-container">
                 <div className="head">TRACK</div>
                 <form className="inputs" onSubmit={handleTrack} >
-                    <Input
+                    {/* <Input
                         value={sender}
                         onChange={(e) => setSender(e.target.value)}
                         className="input"
@@ -104,7 +126,8 @@ const Home = (props) => {
                         placeholder="Enter Time (in seconds)"
                         inputProps={ariaLabel}
                         required
-                    />
+                    /> */}
+                    {getInputs()}
                     <button type="submit">{!loading ? 'TRACK' : 'Tracking....'}</button>
                 </form>
                 <div className="track-container">
@@ -135,6 +158,7 @@ const Home = (props) => {
                                                     <div className="data">Receiver: {data.receiver}</div>
                                                     <div className="data"><div className="key">Message: </div><span className="data" dangerouslySetInnerHTML={{__html: data.msg}}></span></div>
                                                     <div className="data">Time: {data.time&&new Date(parseInt(data.time)).toString()}</div>
+                                                    <img style={{height: '100px', width: '100px'}} className="data" src={data.fileUrl} />
                                                 </div>
                                             </div>
                                         </div>
